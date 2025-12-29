080087c1a99bc41210633b3a2810917341da6a54
t1ger <tigertv112@gmail.com>
2025-12-29 22:47:58 +0000

    added get and set methods for cookies, implemented basic consent system that stores cookies for up to 24 hours, and tied it into the message popup so it is hidden for 24hrs once accepted

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,8 @@
 
-
-const a = true;
-
 window.onload = function() {
 
     const messageBox = document.getElementById("messageContainer");
-    if (a === true){
+    if (getCookie("consentGiven") == "true"){
         messageBox.style.display = "none";
     } else {
         messageBox.style.display = "flex";
@@ -14,4 +11,35 @@ window.onload = function() {
     console.log("Pageload Complete");
 }
 
+
+function consentButtonClicked(){
+    const messageBox = document.getElementById("messageContainer");
+    setCookie("consentGiven", true)
+    messageBox.style.display = "none";
+}
+
+function setCookie(cookieName, cookieValue) {
+  const timeNow = new Date();
+  timeNow.setTime(timeNow.getTime() + (1*24*60*60*1000));
+  let expires = "expires="+ timeNow.toUTCString();
+  document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";";
+}
+
+function getCookie(cookieName) {
+  let name = cookieName + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+console.log(getCookie("consentGiven")); 
 
